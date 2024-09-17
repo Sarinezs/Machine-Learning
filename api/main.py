f627ef23f37f117c5709c2555a665ee233e82f4e
Sarinez <a.edr.edm.ed@gmail.com>
2024-09-17 20:11:50 +0700

    .

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/test")
+async def read_test():
+    return {"message" : "sus"}
